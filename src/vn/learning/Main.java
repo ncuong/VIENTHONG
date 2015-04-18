6a70cb001f3a19de9e25a30884eb9a37df8abde4
Cuong Nguyen <n_cuong@hotmail.com>
2015-04-19 00:24:34 +0700

    Init Project

---
+++ src/vn/learning/Main.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package vn.learning;
+
+import vn.learning.gui.MainFrame;
+
+/**
+ *
+ * @author Admin
+ */
+public class Main {
+    public static void main(String[] args) {
+        MainFrame mainFrame = new MainFrame();
+        mainFrame.setVisible(true);
+        mainFrame.setLocationRelativeTo(null);
+    }
+}
