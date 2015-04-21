7ca972e3e540ec32de81ad9bf2feba6491b90940
Cuong Nguyen <n_cuong@hotmail.com>
2015-04-21 23:58:29 +0700

    modify SimDAO class

--- src/vn/learning/dao/SimDAOImpl.java
+++ src/vn/learning/dao/SimDAOImpl.java
@@ -5,8 +5,10 @@
  */
 package vn.learning.dao;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
+import java.util.Date;
 import vn.learning.model.Sim;
 import vn.learning.util.DataBaseUtil;
 
@@ -17,14 +19,37 @@ import vn.learning.util.DataBaseUtil;
 public class SimDAOImpl implements SimDAO {
 
     private DataBaseUtil dataBaseUtil;
+    private KhachHangDAO khachHangDAO;
+//    private CuocGoiDAO cuocGoiDAO;
+    
 
     @Override
     public ArrayList<Sim> getAllSim() throws SQLException {
         ArrayList<Sim> sims = new ArrayList<>();
-        String sql = new StringBuilder().append("SELECT * FROM KHACH_HANG").toString();
+        String sql = new StringBuilder().append("SELECT * FROM KHACH_HANG").toString();//have to change this line
         try {
             dataBaseUtil = new DataBaseUtil();
             dataBaseUtil.createConnection();
+            
+            ResultSet resultSet = dataBaseUtil.executePreparedStatement(sql);
+            while (resultSet.next()) {
+                Sim sim = new Sim();
+                Integer id = resultSet.getInt("ID");
+                String soThueBao = resultSet.getString("SO_THUE_BAO");
+                Date ngayDangKy = resultSet.getDate("NGAY_DANG_KY");
+                Integer taiKhoanChinh = resultSet.getInt("TAI_KHOAN_CHINH");
+                Integer taiKhoanKhuyenMai = resultSet.getInt("TAI_KHOAN_KHUYEN_MAI");
+                
+                Integer khachHangID = resultSet.getInt("KHACH_HANG_ID");
+                Integer goiCuocID = resultSet.getInt("GOC_CUOC");
+                Integer cuocGoiID = resultSet.getInt(("CUOC_GOI"));
+                
+                
+                
+                
+                sim.setId(id);
+                
+            }
         } catch (SQLException ex) {
             throw ex;
         }
